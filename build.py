8e0e5dc214b8a0ddd823e8d252ba46ccdada886b
Lukas Schauer <lukas@schauer.so>
2014-03-27 02:01:40 +0100

    initial working state

---
+++ build.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from xattr import xattr
+sys.path.append("bplist-python/")
+from bplist import BPlistReader
+from glob import glob
+
+TARGET = "/Users/lukas2511/Projects"
+
+def touch(fname, times=None):
+    with file(fname, 'a'):
+        os.utime(fname, times)
+
+def get_tags(path):
+    try:
+        attrs = xattr(path)
+        kMDItemUserTags = attrs['com.apple.metadata:_kMDItemUserTags']
+        tmptags = BPlistReader(kMDItemUserTags).parse()
+    except:
+        tmptags = []
+    rettags = []
+    for tag in tmptags:
+        rettags.append(tag.split("\n")[0])
+    return rettags
+
+def tag_exists(tagname):
+    return os.path.exists("%s/%s" % (TARGET, tagname))
+
+def create_tag(tagname):
+    print("Creating new tag '%s'" % tagname)
+    os.mkdir("%s/%s" % (TARGET, tagname))
+    touch("%s/%s/.istag" % (TARGET, tagname))
+
+def get_existing_tags():
+    existing_folders = glob("%s/*/.istag" % TARGET)
+    oldtags = []
+    for existing_folder in existing_folders:
+        oldtag = os.path.basename(os.path.dirname(existing_folder))
+        oldtags.append(oldtag)
+    return oldtags
+
+def get_projects(tagname):
+    existing_links = glob("%s/%s/*" % (TARGET, tagname))
+    projects = []
+    for existing_link in existing_links:
+        project = os.path.basename(existing_link)
+        projects.append(project)
+    return projects
+
+def tag_empty(tagname):
+    return glob("%s/%s/*" % (TARGET, tagname)) == []
+
+def remove_empty_tag(tagname):
+    print("Tag '%s' is empty, removing from directory structure" % tagname)
+    os.unlink("%s/%s/.istag" % (TARGET, tagname))
+    stupid_apple_files = ['.DS_Store']
+    for stupid_apple_file in stupid_apple_files:
+        if os.path.exists("%s/%s/%s" % (TARGET, tagname, stupid_apple_file)):
+            os.unlink("%s/%s/%s" % (TARGET, tagname, stupid_apple_file))
+    os.rmdir("%s/%s" % (TARGET, tagname))
+
+def remove_project_from_tag(project, tagname):
+    print("Removing '%s' from '%s'" % (os.path.basename(project), tagname))
+    os.unlink("%s/%s/%s" % (TARGET, tagname, project))
+
+def add_project_to_tag(project, tagname):
+    print("Adding '%s' to '%s'" % (os.path.basename(project), tagname))
+    os.symlink("%s/All/%s" % (TARGET, project), "%s/%s/%s" % (TARGET, tagname, project))
+
+tags = {}
+
+for project_path in glob("%s/All/*" % TARGET):
+    tmptags = get_tags(project_path)
+    project = os.path.basename(project_path)
+    for tag in tmptags:
+        if not tag in tags:
+            tags[tag] = []
+        tags[tag].append(project)
+
+for tagname in get_existing_tags():
+    oldprojects = get_projects(tagname)
+    if not tagname in tags:
+        for project in oldprojects:
+            remove_project_from_tag(project, tagname)
+        if tag_empty(tagname):
+            remove_empty_tag(tagname)
+    else:
+        for project in oldprojects:
+            if not project in tags[tagname]:
+                remove_project_from_tag(project, tagname)
+        for project in tags[tagname]:
+            if not project in oldprojects:
+                add_project_to_tag(project, tagname)
+
+for tagname, projects in tags.iteritems():
+    if not tag_exists(tagname):
+        create_tag(tagname)
+        for project in projects:
+            add_project_to_tag(project, tagname)
